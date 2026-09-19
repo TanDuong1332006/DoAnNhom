fdc2ec7842a70c8a599093c039b036b166c9821c
enzonguyen <188813593+TanDuong1332006@users.noreply.github.com>
2026-09-19 16:05:04 +0700

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/DoAnNhom', // Bắt buộc phải có dòng này (trùng tên repository của bạn)
+  images: {
+    unoptimized: true, // Bắt buộc nếu bạn có dùng thẻ <Image> của Next.js
+  },
 };
 
 export default nextConfig;
